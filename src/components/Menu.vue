e43ce24eca295e634cf5626dcb4fcb82b0daffb6
adrian <adriroya96@gmail.com>
2022-06-17 20:31:13 +0200

    casi terminado menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,36 +1,36 @@
 <template>
   <div id="menuMain">
 
-    <div id="android" @click="isOpen = !isOpen">
+    <div class="principalesMenu" @click="isOpenAndroid = !isOpenAndroid">
       <v-icon large>
         mdi-chevron-right
       </v-icon>
       ANDROID
-      <!--<div :class="{subelementos: isOpen}">
-        <p>Hola</p>
-      </div>-->
-      <div :class="compClasses">
-        <p>Hola</p>
+    </div>
+    <div :class="{subelementos: isOpenAndroid, colorFondo: true}">
+      <div class="subelementos2">
+        <p class="subelementos3">Hola</p>
+        <p class="subelementos3">Hola</p>
+        <p class="subelementos3">Hola</p>
+        <p class="subelementos3">Hola</p>
+        <p class="subelementos3">Hola</p>
       </div>
     </div>
     
-    <div id="web" @click="isOpen = !isOpen">
+    <div class="principalesMenu" @click="isOpenWeb = !isOpenWeb">
       <v-icon large>
         mdi-chevron-right
       </v-icon>
       WEB
-      <div :class="compClasses">
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
-        <p>Hola</p>
+      <div :class="{subelementos: isOpenWeb}">
+        <div class="subelementos2">
+          <p class="subelementos3">Hola</p>
+          <p class="subelementos3">Hola</p>
+          <p class="subelementos3">Hola</p>
+          <p class="subelementos3">Hola</p>
+          <p class="subelementos3">Hola</p>
+          <p class="subelementos3">Hola</p>
+        </div>
       </div>
     </div>
 
@@ -50,34 +50,25 @@
     name: 'menuVertical',
     data (){
       return{
-        isOpen: true //si está a true está escondido
+        isOpenAndroid: true,
+        isOpenWeb: true //si está a true está escondido
       }
     },
-    computed: {
-      compClasses: function(){
-        return {
-          subelementos: this.isOpen,
-
-        }
-      }
-    }
-    
   }
 
 </script>
 
 <style>
   #menuMain{
-    border: 2px solid black;
     width: 100%;
     height: 1000px;
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
-    background-color: whitesmoke;
+    background-color: #0077bf;
   }
 
-  #android{
+  .principalesMenu{
     width: 90%;
     max-height: 300px;
     margin-left: 11px;
@@ -87,35 +78,13 @@
     transition: background-color 0.2s linear;
   }
 
-  #android:hover{
-    background-color: #91a9b1;
-  }
-
-  #web{
-    width: 90%;
-    max-height: 300px;
-    
-    margin-left: 11px;
-    background-color: lightgreen;
-    margin-bottom: 5px;cursor: pointer;
-    transition: background-color 0.2s linear;
-  }
-
-  #web:hover{
-    background-color: #91a9b1;
-  }
-
   #test{
     width: 90%;
     height: 30px;
     margin-left: 11px;
-    background-color: lightcoral;
+    background-color: lightblue;
     cursor: pointer;
     transition: background-color 0.2s linear;
-  }
-
-  #test:hover{
-    background-color: #91a9b1;
   }
 
   .subelementos{
@@ -123,5 +92,24 @@
     max-height: 0;
     overflow: hidden;
     height: 100%;
+  }
+
+  .subelementos2{
+    margin-left: 10%;
+  }
+
+  .subelementos3{
+    margin-bottom: 5px;
+  }
+
+  .subelementos3:hover{
+    background-color: whitesmoke;
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .colorFondo{
+    background-color: lightblue;
+    width: 90%;
   }
 </style>
